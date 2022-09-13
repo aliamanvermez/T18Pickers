2a3940a76c825a4926d5fecafae55f77839660d8
aliamanvermez <maliamanvermez@gmail.com>
2022-09-13 14:10:17 +0300

    Initial Commit

---
+++ T18Pickers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  T18Pickers
+//
+//  Created by NeonApps on 13.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
